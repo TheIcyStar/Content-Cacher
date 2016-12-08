0ed5c2ac1c93f4fdc1d61fbc004e71826227cd48
TheIcyStar <sshptrv@gmail.com>
2016-12-08 12:31:55 -0500

    Add downloader class

---
+++ Core/Downloader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Content_Cacher.Core {
+	class Downloader {
+		public void StartDownload() {
+			//get next item in queue
+			//get the download url of item
+			//send url to downloader
+			//update progress in ui
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="url">URL of file to download</param>
+		private void DownloadFile(string url, string savePath ) {
+			string UserPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+			string pathDownload = Path.Combine(UserPath, "Downloads");
+		}
+	}
+}
