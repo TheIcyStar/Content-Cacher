eb862e115cecf471c4499a7ac5e2d89a8d0ebde8
TheIcyStar <sshptrv@gmail.com>
2016-12-18 22:25:04 -0500

    add Queue class

---
+++ Core/Queue.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Content_Cacher.Core {
+	class Queue {
+
+
+		public void AddToQueue() {
+
+		}
+
+		public void RemoveFromQueue(int index) {
+
+		}
+
+		public void GetQueueList() {
+
+		}
+	}
+}
